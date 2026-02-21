51831447de6b03da0c0b862c8d1a997ebc94cdf5
Krs95xz <kaua.rb.sales@gmail.com>
2026-02-21 15:49:37 -0300

    Ajustando lógica das seções no JS

--- scripts/scriptAdmin.js
+++ scripts/scriptAdmin.js
@@ -8,6 +8,7 @@ const abrirSala = document.getElementById("view-salas");
 const abrirVinculacao = document.getElementById("view-vinculacoes");
 
 // Seções
+const secoes = document.getElementsByClassName("secoes");
 const secaoAluno = document.getElementById("secao-aluno");
 const secaoProfessor = document.getElementById("secao-professor");
 const secaoTurma = document.getElementById("secao-turma");
@@ -21,82 +22,70 @@ abrirAluno.addEventListener("click", () => {
     views[i].style.background = "transparent";
     views[i].style.color = "white";
   }
+  for (let i = 0; i < secoes.length; i++) {
+    secoes[i].style.display = "none";
+  }
   abrirAluno.style.background = "white";
   abrirAluno.style.color = "var(--corAzul)";
   secaoAluno.style.display = "block";
-  secaoProfessor.style.display = "none";
-  secaoTurma.style.display = "none";
-  secaoDisciplina.style.display = "none";
-  secaoSala.style.display = "none";
-  secaoVinculacao.style.display = "none";
 });
 abrirProfessor.addEventListener("click", () => {
   for (let i = 0; i < views.length; i++) {
     views[i].style.background = "transparent";
     views[i].style.color = "white";
   }
+  for (let i = 0; i < secoes.length; i++) {
+    secoes[i].style.display = "none";
+  }
   abrirProfessor.style.background = "white";
   abrirProfessor.style.color = "var(--corAzul)";
-  secaoAluno.style.display = "none";
   secaoProfessor.style.display = "block";
-  secaoTurma.style.display = "none";
-  secaoDisciplina.style.display = "none";
-  secaoSala.style.display = "none";
-  secaoVinculacao.style.display = "none";
 });
 abrirTurma.addEventListener("click", () => {
   for (let i = 0; i < views.length; i++) {
     views[i].style.background = "transparent";
     views[i].style.color = "white";
   }
+  for (let i = 0; i < secoes.length; i++) {
+    secoes[i].style.display = "none";
+  }
   abrirTurma.style.background = "white";
   abrirTurma.style.color = "var(--corAzul)";
-  secaoAluno.style.display = "none";
-  secaoProfessor.style.display = "none";
   secaoTurma.style.display = "block";
-  secaoDisciplina.style.display = "none";
-  secaoSala.style.display = "none";
-  secaoVinculacao.style.display = "none";
 });
 abrirDisciplina.addEventListener("click", () => {
   for (let i = 0; i < views.length; i++) {
     views[i].style.background = "transparent";
     views[i].style.color = "white";
   }
+  for (let i = 0; i < secoes.length; i++) {
+    secoes[i].style.display = "none";
+  }
   abrirDisciplina.style.background = "white";
   abrirDisciplina.style.color = "var(--corAzul)";
-  secaoAluno.style.display = "none";
-  secaoProfessor.style.display = "none";
-  secaoTurma.style.display = "none";
   secaoDisciplina.style.display = "block";
-  secaoSala.style.display = "none";
-  secaoVinculacao.style.display = "none";
 });
 abrirSala.addEventListener("click", () => {
   for (let i = 0; i < views.length; i++) {
     views[i].style.background = "transparent";
     views[i].style.color = "white";
   }
+  for (let i = 0; i < secoes.length; i++) {
+    secoes[i].style.display = "none";
+  }
   abrirSala.style.background = "white";
   abrirSala.style.color = "var(--corAzul)";
-  secaoAluno.style.display = "none";
-  secaoProfessor.style.display = "none";
-  secaoTurma.style.display = "none";
-  secaoDisciplina.style.display = "none";
   secaoSala.style.display = "block";
-  secaoVinculacao.style.display = "none";
 });
 abrirVinculacao.addEventListener("click", () => {
   for (let i = 0; i < views.length; i++) {
     views[i].style.background = "transparent";
     views[i].style.color = "white";
   }
+  for (let i = 0; i < secoes.length; i++) {
+    secoes[i].style.display = "none";
+  }
   abrirVinculacao.style.background = "white";
   abrirVinculacao.style.color = "var(--corAzul)";
-  secaoAluno.style.display = "none";
-  secaoProfessor.style.display = "none";
-  secaoTurma.style.display = "none";
-  secaoDisciplina.style.display = "none";
-  secaoSala.style.display = "none";
   secaoVinculacao.style.display = "block";
 });
