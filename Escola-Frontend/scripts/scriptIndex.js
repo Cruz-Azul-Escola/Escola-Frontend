9351cb2b9a46a196f698b21bfc6d7971190f7f70
Krs95xz <kaua.rb.sales@gmail.com>
2026-02-23 06:56:05 -0300

    Removendo repositório duplicado

--- Escola-Frontend/scripts/scriptIndex.js
+++
@@ -1,7 +0,0 @@
-let clickes = 0;
-document.getElementById("button-area-restrita").addEventListener("click", () => {
-    clickes ++;
-    if (clickes >= 5) {
-        window.location.href = "../pages/areaRestritaLogin.html";
-    }
-})
